6cff03b49a48498b3b0a259a0dd60453c5d864ba
hironakazuki <43399066+hironakazuki@users.noreply.github.com>
2020-04-14 01:19:27 +0900

    Part5 (#4)
    * learn 5a & 5b practice
    
    * learn 5b LatterHalf practice
    
    * learn 5c practice
    
    * learn 5d practice

---
+++ src/components/NoteForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const NoteForm = ({ createNote }) => {
+  const [newNote, setNewNote] = useState('')
+
+  const handleChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    createNote({
+      content:newNote,
+      important: false
+    })
+
+    setNewNote('')
+  }
+  return (
+    <div>
+      <h2>Create a new note</h2>
+
+      <form onSubmit={addNote}>
+        <input
+          id="content"
+          value={newNote}
+          onChange={handleChange}
+          placeholder='a new note.'
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default NoteForm
