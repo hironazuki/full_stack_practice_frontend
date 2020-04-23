b2ac894ab2fd05ec7674b5c2e452a2e22b2f7da1
hironakazuki <43399066+hironakazuki@users.noreply.github.com>
2020-04-24 02:15:34 +0900

    Part7 (#11)
    * learn 7a practice
    
    * learn 7b practice
    
    * learn 7c practice
    
    * learn 7d practice
    
    * learn 7e practice

---
+++ part7/webpack-part7d/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const config = (env, argv) => {
+  console.log('argv', argv.mode)
+
+  const backend_url = argv.mode === 'production'
+    ? 'https://blooming-atoll-75500.herokuapp.com/api/notes'
+    : 'http://localhost:3001/notes'
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'main.js'
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'build'),
+      compress: true,
+      port: 3000,
+    },
+    devtool: 'source-map',
+    module: {
+      // ...
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url)
+      })
+    ]
+  }
+}
+
+module.exports = config
